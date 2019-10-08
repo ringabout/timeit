2c99f90f28d470c6a25aede053520bc14015ce45
flywind <xzsflywind@gmail.com>
2019-10-08 12:01:26 +0800

    timeit

--- src/timeit.nim
+++ src/timeit.nim
@@ -1,7 +1,12 @@
-# This is just an example to get you started. A typical library package
-# exports the main API in this file. Note that you cannot rename this file
-# but you can remove it if you wish.
+import times, os
 
-proc add*(x, y: int): int =
-  ## Adds two files together.
-  return x + y
+template timeIt(myFunc: untyped): float = 
+  let time = cpuTime()
+  myFunc()
+  cpuTime() - time
+
+proc mySleep() = 
+  sleep(100)
+
+echo timeIt(mySleep)
+  
